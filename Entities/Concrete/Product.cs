8ef9137f901a788e5c71bad5188a3271b316ea38
Mustafa Karaman <mustafakaraman023302@gmail.com>
2022-02-24 15:53:03 +0300

    24.02.22 Entity classları oluşturuldu.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Product : EntityBase
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public decimal SalePrice { get; set; }
+        public decimal DiscountPrice { get; set; }
+        public DateTime? AddedDate { get; set; }
+        public int? UnitsInstock { get; set; }
+        public Guid SubCategoryId { get; set; }
+
+        public SubCategory SubCategory { get; set; }
+        public ICollection<Image> Images { get; set; }
+    }
+}
---
+++ Entities/Concrete/SubCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class SubCategory:EntityBase
+    {
+        public string SubCategoryName { get; set; }
+        public Guid? CategoryId { get; set; }
+
+        public Category Category { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
