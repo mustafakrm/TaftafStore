a41a36211dcd36877820e94917e0cee74b4bdabb
Mustafa Karaman <mustafakaraman023302@gmail.com>
2022-02-26 17:35:49 +0300

    05:35 PM 26.02.22

---
+++ Core/DependencyResolvers/CoreModule.cs
@@ -0,0 +1,21 @@
+﻿using Core.CrossCuttingConcerns.Caching;
+using Core.CrossCuttingConcerns.Caching.Microsoft;
+using Core.Utilities.IoC;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
+
+namespace Core.DependencyResolvers
+{
+    public class CoreModule : ICoreModule
+    {
+        public void Load(IServiceCollection serviceCollection)
+        {
+
+            serviceCollection.AddMemoryCache();// .Net core'un kendi injection'ı
+            serviceCollection.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            serviceCollection.AddSingleton<ICacheManager, MemoryCacheManager>();
+            serviceCollection.AddSingleton<Stopwatch>();
+        }
+    }
+}
