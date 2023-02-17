6614d3886dab6347b72b9aec22184ab9a4d4ca9f
Mustafa Karaman <mustafakaraman023302@gmail.com>
2023-02-17 23:48:38 +0300

    2

--- WebAPI/Controllers/CategoriesController.cs
+++ WebAPI/Controllers/CategoriesController.cs
@@ -32,7 +32,7 @@ namespace WebAPI.Controllers
             var result = _categoryService.GetById(categoryId);
             if (result.Success)
             {
-                return Ok(result);
+                return Ok(result.Data);
             }
             return BadRequest(result);
         }
